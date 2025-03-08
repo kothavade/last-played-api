81fd6e095e8cdacfad9dedc522f4feaba1d611ea
Ved Kothavade <ved@kothavade.com>
2025-03-08 18:33:30 -0500

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# last played api
+
+simple api for getting the last played song from youtube music, built for [kothavade.com](https://kothavade.com).
+
+## tech
+- heavy lifting done by [ytmusicapi](https://github.com/sigma67/ytmusicapi)
+- deployed with [vercel](https://vercel.com) functions
+- redis from [upstash](https://upstash.com)
+
+## run locally
+```sh
+python3 api/index.py
+```
---
+++ api/index.py
@@ -0,0 +1,72 @@
+from flask import Flask, request
+from flask_cors import CORS
+from upstash_redis import Redis
+import json
+import os
+
+vercel = "VERCEL" in os.environ
+if not vercel:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+env = os.environ
+
+OAUTH_JSON = f"""{{
+"scope": "https://www.googleapis.com/auth/youtube",
+"token_type": "Bearer",
+"access_token": "{env['OAUTH_ACCESS_TOKEN']}",
+"refresh_token": "{env['OAUTH_REFRESH_TOKEN']}",
+"expires_at": 1741474523,
+"expires_in": 3599
+}}"""
+LAST_PLAYED_KEY = "last_played"
+
+redis = Redis(
+    url=env["REDIS_URL"],
+    token=env["REDIS_TOKEN"],
+)
+app = Flask(__name__)
+CORS(app)
+
+
+def get_ytmusic_last_played():
+    from ytmusicapi import YTMusic, OAuthCredentials
+
+    ytmusic = YTMusic(
+        OAUTH_JSON,
+        oauth_credentials=OAuthCredentials(
+            client_id=env["OAUTH_CLIENT_ID"],
+            client_secret=env["OAUTH_CLIENT_SECRET"],
+        ),
+    )
+    history = ytmusic.get_history()
+    last_played = history[0]
+    return json.dumps(
+        {
+            "title": last_played["title"],
+            "artist": ", ".join([artist["name"] for artist in last_played["artists"]]),
+            "album": last_played["album"]["name"],
+            "thumbnail": last_played["thumbnails"][-1]["url"],
+        }
+    )
+
+
+@app.route("/")
+def api():
+    force_check = request.args.get("force", "false").lower() == "true"
+    cached = redis.get(LAST_PLAYED_KEY)
+    if cached is None or force_check:
+        if force_check:
+            print("Force fetching last played from YTMusic")
+        else:
+            print("Fetching last played from YTMusic")
+        last_played = get_ytmusic_last_played()
+        redis.set(LAST_PLAYED_KEY, last_played, ex=30)
+        return last_played
+    print("Using cached last played")
+    return cached
+
+
+if not vercel:
+    app.run()
